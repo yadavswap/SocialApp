bf4492d1bcd9b2cf04fe097bdd24e1241946d825
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2023-03-01 18:37:20 +0530

    chnage nodemon to node

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const format = require('date-format')
-const PORT = 4000 || process.env.PORT
-
+const PORT = process.env.PORT || 4000
 app.get('/',(req,res)=>{
     res.send("world");
 })
